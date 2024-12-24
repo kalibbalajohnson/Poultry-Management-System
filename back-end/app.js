7463fc02c01653a8991462a50b67619324fc23eb
Johnson11 <kalijoe11@gmail.com>
2024-12-24 20:25:02 +0300

    initial commit

--- back-end/app.js
+++ back-end/app.js
@@ -7,7 +7,6 @@ const connectToMongoDB = require("./config/db");
 const swaggerJsdoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
 
-
 const port = process.env.PORT || 3001;
 const farmRoutes = require("./routes/farmRoutes");
 
