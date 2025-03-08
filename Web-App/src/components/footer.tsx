9b7d73e39443b54f39ebb49049763f43fdba376d
Johnson11 <kalijoe11@gmail.com>
2025-03-08 20:33:23 +0300

    updates

--- Web-App/src/components/footer.tsx
+++ Web-App/src/components/footer.tsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer className="bg-gray-800 py-4 text-center text-white">
       <p className="text-sm">
-        &copy; 2025 Poultry Farm Management. All rights reserved.
+        &copy; 2025 PoultryPal BSE25-9. All rights reserved.
       </p>
     </footer>
   );
